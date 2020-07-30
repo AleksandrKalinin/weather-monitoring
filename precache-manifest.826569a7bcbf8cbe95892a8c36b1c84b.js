35f077c9d93c99f28ba695e6c37186afa389d6f0
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2020-07-31 01:22:24 +0300

    Updates

---
+++ precache-manifest.826569a7bcbf8cbe95892a8c36b1c84b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b0c09d3119255e24add",
+    "url": "/weather-monitoring/static/css/main.4035d092.chunk.css"
+  },
+  {
+    "revision": "8b0c09d3119255e24add",
+    "url": "/weather-monitoring/static/js/main.8b0c09d3.chunk.js"
+  },
+  {
+    "revision": "dbdd0e02cab693d1f525",
+    "url": "/weather-monitoring/static/js/runtime~main.dbdd0e02.js"
+  },
+  {
+    "revision": "9d3377ba6e016efe32e3",
+    "url": "/weather-monitoring/static/css/2.2369a193.chunk.css"
+  },
+  {
+    "revision": "9d3377ba6e016efe32e3",
+    "url": "/weather-monitoring/static/js/2.9d3377ba.chunk.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/weather-monitoring/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/weather-monitoring/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/weather-monitoring/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/weather-monitoring/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/weather-monitoring/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/weather-monitoring/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/weather-monitoring/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/weather-monitoring/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/weather-monitoring/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/weather-monitoring/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/weather-monitoring/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/weather-monitoring/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/weather-monitoring/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/weather-monitoring/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/weather-monitoring/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/weather-monitoring/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "087e0c76276066ba08c072abbcc15664",
+    "url": "/weather-monitoring/index.html"
+  }
+];
